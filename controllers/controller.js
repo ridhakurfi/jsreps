05848670e69036bd134f277025880abb05ec9df1
ridhakurfi <ridhakurfi@gmail.com>
2026-04-09 19:17:31 +0700

    9

--- controllers/controller.js
+++ controllers/controller.js
@@ -75,9 +75,9 @@ class Controller {
     try {
       await Model.deleteById(req.params.id);
       res.redirect("/students");
-    } catch (tomboy) {
-      console.log(tomboy);
-      res.send(tomboy);
+    } catch (mermaid) {
+      console.log(mermaid);
+      res.send(mermaid);
     }
   }
 }
